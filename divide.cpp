e7e8b632d3d3494e4523847a68bee0cad7c45385
taeyeon112 <xodus678@naver.com>
2022-10-04 20:52:53 +0900

    test

---
+++ divide.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    int i = 1;
+    
+    cout << "정수를 입력하시오: ";
+    cin >> n;
+
+    cout << "N=" << n << "출력:";
+    while (n * i <= 100)
+    {
+        cout << n * i << " ";
+        i++;
+    }
+
+    return 0;
+}
